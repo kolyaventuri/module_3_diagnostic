eee356aa1603755be3a20ef413ce7b392322ece1
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-16 10:07:09 -0600

    NRELService can make requests

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,3 +1,22 @@
 class NRELService
+  def self.make_request(endpoint, params)
+    params[:format] = 'JSON'
+    conn = Faraday.new(url: build_url(endpoint, params))
 
+    response = conn.get do |req|
+      req.headers['X-API-Key'] = ENV['NREL_API_KEY']
+    end
+
+    return false unless response.success?
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  API_URL = 'https://developer.nrel.gov/api'
+
+  def self.build_url(endpoint, params)
+    "#{API_URL}#{endpoint}?#{params.to_query}"
+  end
 end
