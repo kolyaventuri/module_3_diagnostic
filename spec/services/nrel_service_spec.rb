bdfc88c33ea55f0df6be28154ff323433aa4b9ce
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-16 09:59:45 -0600

    NRELService make request test

--- spec/services/nrel_service_spec.rb
+++ spec/services/nrel_service_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe NRELService do
   it 'should be able to make API requests' do
+    VCR.use_cassette('nrel_api_test') do
+      response = NRELService::make_request('/alt-fuel-stations/v1.json', {limit: 1})
 
+      expect(response[:fuel_stations]).to be_an Array
+    end
   end
 end
