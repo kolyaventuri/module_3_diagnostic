e0cf78cb4ac61dd8f0002609fa8e562b68da5bfa
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-16 09:29:10 -0600

    BDD feature tests done

--- spec/features/user_can_search_within_radius_spec.rb
+++ spec/features/user_can_search_within_radius_spec.rb
@@ -11,6 +11,7 @@ contet 'As a user, when I visit \'/\'' do
   context 'and fill in the search form (using 80203) and click locate' do
     let(:zip) { '80203' }
     let(:valid_types) { ['Electric', 'Propane'] }
+    let(:attributes) { ['name', 'address', 'fuel-type', 'distance', 'access-times'] }
 
     scenario 'I should see the 10 closest stations in 6 miles, sorted by distance' do
       visit '/'
@@ -24,12 +25,51 @@ contet 'As a user, when I visit \'/\'' do
         stations = find_all('tr')
 
         expect(stations.length).to be(10)
+        last = -1
+
+        stations.each do |station|
+          within(station) do
+            distance = find('[data-attribute="distance"]').gsub(/\d*\.\d*/, '').to_f
+            expect(last).to be < distance
+            last = distance
+          end
+        end
+      end
+    end
+
+    scenario 'the stations should be limited to Electric and Propane' do
+      visit '/'
+
+      fill_in 'q', with: zip
+      click_on 'Locate'
+
+      within('.stations .tbody') do
+        stations = find_all('tr')
 
         stations.each do |station|
           within(station) do
             type = find('[data-attribute="fuel-type"]')
 
             expect(type.text).to include(page.body)
+          end
+        end
+      end
+    end
+
+    scenario 'I should see the stations name, address, fuel types, distance, and access times' do
+      visit '/'
+      fill_in 'q', with: zip
+      click_on 'Locate'
+
+      within('.stations .tbody') do
+        stations = find_all('tr')
+
+        stations.each do |station|
+          within(station) do
+            attributes.each do |attribute|
+              data = find("[data-attribute='#{attribute}']")
+              expect(data.text).to_not be_empty
+            end
           end
         end
       end
